b98a5a8ad6e01a04f1fbe62f691e9f6ba6d61184
PriyanshuGajbhiye <g.priyanshu1212@gmail.com>
2023-04-12 20:51:40 +0530

    added files

--- NotetakingApp/src/index.js
+++
@@ -1,7 +0,0 @@
-//jshint esversion: 6
-
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/app";
-
-ReactDOM.render(<App />, document.getElementById("root"););
