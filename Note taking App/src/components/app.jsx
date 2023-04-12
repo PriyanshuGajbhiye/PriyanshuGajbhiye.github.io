69ac5d466d1acc93cd1fd9da1570c7af257f07e3
PriyanshuGajbhiye <g.priyanshu1212@gmail.com>
2023-04-12 20:37:08 +0530

    added files

---
+++ Note taking App/src/components/app.jsx
@@ -0,0 +1,25 @@
+//jshint esversion: 6
+
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Notes from "./notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
